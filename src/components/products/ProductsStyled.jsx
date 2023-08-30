e73b86d3d7a45a230adf36741a82e92f093e01b2
danko_Bo <krekhovecjob@gmail.com>
2023-08-30 14:55:05 +0300

    change styles

--- src/components/products/ProductsStyled.jsx
+++ src/components/products/ProductsStyled.jsx
@@ -4,6 +4,5 @@ const Wrapper = styled.div`
 	width: 1065px;
 	height: 640px;
 	margin: 0 auto 207px;
-	border: 1px solid red;
 `
 export default Wrapper
--- src/components/products/productsMenu/ProductsMenuStyled.jsx
+++ src/components/products/productsMenu/ProductsMenuStyled.jsx
@@ -3,6 +3,5 @@ import styled from 'styled-components'
 const Wrapper = styled.div`
 	width: 373px;
 	height: 640px;
-	border: 1px solid green;
 `
 export default Wrapper
